9073d93bef834a86307309199e4c8c9a0f5e4185
zhanghongbing <zhanghongbing@dangdang.com>
2022-08-09 10:45:06 +0800

    commit juc atomic class test

---
+++ java-guide/src/main/java/cn/baymax/example/_thread/atomic/SolveABAByAtomicMarkableReference.java
@@ -0,0 +1,39 @@
+package cn.baymax.example._thread.atomic;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+/**
+ * @author zhanghongbing
+ * @data 2022/7/13
+ */
+public class SolveABAByAtomicMarkableReference {
+    private static AtomicMarkableReference atomicMarkableReference = new AtomicMarkableReference(100, false);
+
+    public static void main(String[] args) {
+
+        Thread refT1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            atomicMarkableReference.compareAndSet(100, 101, atomicMarkableReference.isMarked(), !atomicMarkableReference.isMarked());
+            atomicMarkableReference.compareAndSet(101, 100, atomicMarkableReference.isMarked(), !atomicMarkableReference.isMarked());
+        });
+
+        Thread refT2 = new Thread(() -> {
+            boolean marked = atomicMarkableReference.isMarked();
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean c3 = atomicMarkableReference.compareAndSet(100, 101, marked, !marked);
+            System.out.println(c3); // 返回true,实际应该返回false
+        });
+
+        refT1.start();
+        refT2.start();
+    }
+}
